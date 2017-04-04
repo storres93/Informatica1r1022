07a287ec90ad0a1d91054b3f273f52d0ad705ccc
Sebastian Torres <storres@medallia.com>
2017-04-03 23:52:16 -0300

    Add third excercise

---
+++ 04:04:2017/Ejercicio 3/header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define TAMANO_VECTOR 10
+
+void cargarVectorDeInt(int[], int);
+void bubbleSortAscendente(int[], int);
+void insertionSortAscendente(int[], int);
+void quickSortAscendente(int[], int, int);
+int partition(int[], int, int);
+
---
+++ 04:04:2017/Ejercicio 3/main.c
@@ -0,0 +1,113 @@
+#include "header.h"
+
+int main(void)
+{
+	int numeros[TAMANO_VECTOR], i;
+
+	cargarVectorDeInt(numeros, TAMANO_VECTOR);
+
+	quickSortAscendente(numeros, 0, TAMANO_VECTOR - 1);
+
+	for(i = 0; i < TAMANO_VECTOR; i++)
+	{
+		printf("El elemento %d equivale a %d\n", i, numeros[i]);
+	}
+
+	return 0;
+}
+
+void cargarVectorDeInt(int vector[], int tamano)
+{
+	int i;
+
+	for(i = 0; i < TAMANO_VECTOR; i++)
+	{
+		printf("Ingrese el elemento %d\n", (i + 1));
+		scanf("%d", &vector[i]);
+	}
+}
+
+void bubbleSortAscendente(int vector[], int tamano)
+{
+	int i, j, temp;
+
+	for(i = 0; i < TAMANO_VECTOR; i++)
+	{
+		for(j = i + 1; j < TAMANO_VECTOR; j++)
+		{
+			if(vector[i] > vector[j])
+			{
+				temp = vector[i];
+				vector[i] = vector[j];
+				vector[j] = temp;
+			}
+		}
+	}
+}
+
+//Este algoritmo basicamente agarra un numero y se va fijando todos los anteriores hasta que encuentra uno menor y cambia su poscicion por ese
+void insertionSortAscendente(int vector[], int tamano)
+{
+	int i, j, temp;
+	for(i = 1; i < tamano; i++)
+	{
+		temp = vector[i];
+		j = i-1;
+		while(j>=0 && temp < vector[j])
+		{
+			vector[j+1] = vector[j];
+			j--;
+		}
+		vector[j+1] = temp;
+	}
+}
+
+void imprimirArray(int vector[], int tamano)
+{
+	int i;
+
+	for(i = 0; i < tamano; i++)
+	{
+		printf("El elemento %d equivale a %d\n", i, vector[i]);
+	}
+}
+
+/*
+   Este metodo de sorting agarra el array y lo va partiendo a la mitad desde un pivot
+   poniendo todos los numeros menores a la izquierda del pivot y los mayores a la derecha
+ */
+void quickSortAscendente(int vector[], int pivot, int lastIndex)
+{
+	if(pivot < lastIndex)
+	{
+		int pivotSecundario;
+		pivotSecundario = partition(vector, pivot, lastIndex);
+		quickSortAscendente(vector, pivot, pivotSecundario);
+		quickSortAscendente(vector, pivotSecundario+1, lastIndex);
+	}
+}
+
+int partition(int vector[], int pivotDado, int lastIndex)
+{
+	int i, j, pivot, temp;
+	pivot = vector[pivotDado];
+	i = pivotDado;
+	j = lastIndex;
+	while(1)
+	{
+		while(vector[i] < pivot && vector[i] != pivot)
+			i++;
+		while(vector[j] > pivot && vector[j] != pivot)
+			j--;
+		if(i < j)
+		{
+			temp = vector[i];
+			vector[i] = vector[j];
+			vector[j] = temp;
+		}
+		else
+		{
+			return j;
+		}
+	}
+}
